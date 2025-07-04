74e544f1af46ddddb59af5f0148f2d35ea9f4cce
Eduardo dos Santos Sousa <eduardosousa.eds@gmail.com>
2025-07-04 01:20:31 -0300

    Update custom_exception.py

--- utils/custom_exception.py
+++ utils/custom_exception.py
@@ -10,15 +10,15 @@ class CustomException(Exception):
 
     @staticmethod
     def get_detailed_error_message(erro_message, erro_detail:sys):
-        #Obtendo o tipo de exceção, o valor e traceback
+        #Getting the exception type, value and traceback
         _,_, exc_tb = traceback.sys.exc_info()
         file_name = exc_tb.tb_frame.f_code.co_filename
         line_number = exc_tb.tb_lineno
 
-        #capturar o traceback completo
+        #capture the full traceback
         tb_str = "". join(traceback.format_exception(*sys.exc_info()))
 
-        #Incluindo o traceback completo na mensagem
+       #Including the full traceback in the message
         return f"Error in {file_name}, line {line_number}: {erro_message} \n\n Original Traceback:\n{tb_str}"
 
     def __str__(self):
